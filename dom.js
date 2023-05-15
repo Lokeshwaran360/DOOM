e7e0c8edda0daa8e0ca8d589d0509b65045c7e50
Lokeshwaran360 <106175109+Lokeshwaran360@users.noreply.github.com>
2023-05-15 09:50:17 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,3 @@
+var head = document.getElementById('header-title');
+head.style.borderBottom = 'solid 3px #000';
+[...document.getElementsByClassName("title")].forEach((ele)=>{ele.style.color = 'green';});
